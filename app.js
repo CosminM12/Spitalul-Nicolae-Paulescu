57959d0dca67c85a7bcf324b25eb7b56206c4955
COSMIN MURARIU <cosmin.murariu1@gmail.com>
2024-01-25 11:22:31 +0200

    Pages, res, routing, done db work

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+///////DEFINITIONS
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -9,13 +10,17 @@ const dotenv = require('dotenv');
 const flash = require('connect-flash');
 const session = require('express-session');
 
+///////Database Configuration
 const connectDB = require('./config/database.js');
 
+///////Schema Configuration
 const User = require("./app/models/user.js");
 const Appointment = require('./app/models/appointment.js');
 
+///////Port Configuration
 var port = process.env.PORT || 3000;
 
+///////SET UP MIDDLEWARES///////
 const app = express(); //open server
 app.use(express.json()); //use middleware
 connectDB(); //connect to database
@@ -27,19 +32,18 @@ app.use(session({
 }));
 app.use(flash());
 app.use(cookieParser());
-
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
 
+
+///////FUNCTION DECLARATIONS///////
+const {authenticate, isLoggedIn} = require("./config/specialAuth.js");
+
+///////SIGN IN / SIGN UP SPECIAL METHODS///////
 app.use("/api/auth", require("./app/routes.js"));
 
-
-const {authenticate, authAndLogIn} = require("./config/specialAuth.js");
-const {isLoggedIn} = require("./config/specialAuth.js");
-// const {getAppointments, postAppointment} = require("./config/appointmentSystem.js");
-
-
+///////FETCH DATA///////
+///////Get methods
 app.get("/api/doctors", async (req, res) => {
     try {
         const {department} = req.query;
@@ -64,19 +68,38 @@ app.get("/api/appointments", async (req, res) => {
     }
 });
 
-/*app.get("/api/doctor/apppointments", async (req, res) => {
-    try {
-        const {doctorId} = req.query;
-        if(!doctorId) {
-            return res.status(400).json({error: 'Missing required paramenter'});
-        }
-        const appointments = await Appointment.find({doctorId: doctorId});
-        res.json(appointments);
-    } catch(err) {
-        res.status(500).json({error: err.message});
-    }
-});*/
+// app.get("/api/appointments/user", async(req, res) => {
+//     try {
+//         const {currentUserName} = req.query;
+//         const decodedUserName = decodeURIComponent(currentUserName);
+//         if(!decodedUserName) {
+//             return res.status(400).json({error: 'Missing userId in appointmnet'});
+//         }
+//         const parts = decodedUserName.split(' ');
+//         const firstName = parts[0];
+//         const lastName = parts[1];
+//         const user = await User.findOne({firstName, lastName});
+//         res.json(user);
+//     }
+//     catch(err) {
+//         res.status(500).json({error: err.message});
+//     }
+// });
 
+// app.get("/api/appointments/doctors", async (req, res) => {
+//     try {
+//         const doctorId = req.query;
+//         if(!doctorId) {
+//             return res.status(400).json({error: 'Missing doctorId in appointment'});
+//         }
+//         const doctor = await User.findById(doctorId);
+//         res.json(doctor);
+//     } catch(err) {
+//         res.status(500).json({error: err.message});
+//     }
+// });
+
+///////Post methods
 app.post("/api/appointments", authenticate, isLoggedIn, async (req, res) => {
     try {
         const {doctorId, userId, date, time, message, telephone} = req.body;
@@ -91,7 +114,6 @@ app.post("/api/appointments", authenticate, isLoggedIn, async (req, res) => {
 
         await newAppointment.save();
         res.redirect("/");
-        // res.status(201).json({success: true, message: 'Appointment saved successfully'});
     } catch(err) {
         console.error('Error creating and saving the appointment: ', err);
         res.status(500).json({success: false, error: 'Failed to create and save appointment'});
@@ -105,34 +127,75 @@ app.get("/", authenticate, (req, res) => {
     res.render("home.ejs", {role: req.user ? req.user.role : null});
 });
 app.get("/programare", authenticate, isLoggedIn, (req, res) => {
-    res.render("programare.ejs", {user: req.user || null});
+    res.render("programare.ejs", {role : req.user ? req.user.role : null, user: req.user || null});
 });
 
 app.get("/misiune", authenticate,(req, res) => {
     res.render("misiune.ejs", {role: req.user ? req.user.role : null});
-})
-
-app.get("/register", (req, res) => res.render("register.ejs"));
-app.get("/login", (req, res) => {
-    res.render('login.ejs');
 });
 
-///PROFILE PAGES
+app.get("/anunturi", authenticate, (req, res) => {
+    res.render("anunturi.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/concursuri", authenticate, (req, res) => {
+    res.render("concursuri.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/departamente", authenticate, (req, res) => {
+    res.render("departamente.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/imagini", authenticate, (req, res) => {
+    res.render("slideshow.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/cardiologie", authenticate, (req, res) => {
+    res.render("cardiologie.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/neurologie", authenticate, (req, res) => {
+    res.render("neurologie.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/chirurgie", authenticate, (req, res) => {
+    res.render("chirurgie.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/contact", authenticate, (req, res) => {
+    res.render("contact.ejs", {role: req.user ? req.user.role : null});
+});
+
+app.get("/register", (req, res) => res.render("register.ejs"));
+app.get("/login", (req, res) => res.render('login.ejs'));
+
+///////PROFILE PAGES///////
 app.get("/doctor", authenticate, isLoggedIn, async (req, res) => {
     try {
-        console.log(req.user);
-        const doctorId = req.user._id;
-        const appointments = await Appointment.find({doctorId});
+        const doctorId = req.user.id;
+        const loggedUser = await User.findById(doctorId);
+        
+        const doctorName = loggedUser.firstName + " " + loggedUser.lastName;
+        const appointments = await Appointment.find({doctorId: doctorName});
 
-        res.render("doctor_profile.ejs", {user: req.user || null, appointments});
+        res.render("doctor_profile.ejs", {loggedUser, appointments});
     } catch(err) {
         console.error('Error fetching and rendering appointments:', err);
         res.status(500).json({success: false, error: 'Failed to fetch and render appointments'});
     }
 });
 
-// app.get("/admin", adminAuth, (req, res) => res.render('admin.ejs'));
-// app.get("/basic", userAuth, (req, res) => res.render('user.ejs'));
+app.get("/admin", authenticate, isLoggedIn, async (req, res) => {
+    try {
+        const adminId = req.user.id;
+        const appointments = await Appointment.find({});
+        const loggedUser = await User.findById(adminId);
+        res.render("admin_profile.ejs", {loggedUser, appointments});
+    } catch (err) {
+        console.error('Error fetching and rendering appointments: ', err);
+        res.status(500).json({success: false, error: 'Failed to fetch and render appointments'});
+    }
+});
 
 
 
